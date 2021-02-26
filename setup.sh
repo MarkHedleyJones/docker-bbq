d9c17a8ac001198f0d0aa1122f3d11984b36b8c6
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-02-26 10:42:58 +0900

    Combine run and enter scripts and modify setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,6 @@
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-scripts=(
-  enter
-  run
-)
-
 paths_to_try=(
   /home/${USER}/.local/bin
   /home/${USER}/bin
@@ -15,20 +10,24 @@ paths_to_try=(
 for path in ${paths_to_try[@]}; do
   echo "Checking for local binary path at ${path} ... "
   if [[ -d ${path} ]]; then
-    for script in ${scripts[@]}; do
-      if [[ -f ${path}/${script} ]]; then
-        echo "Warning - Script '${script}' already exists at this location!"
-      else
-        ln -s ${script_dir}/bin/${script} ${path}/${script}
-        echo "Created link to ${script}"
-      fi
-    done
+    if [[ -f ${path}/run ]]; then
+      echo "The 'run' script is already exists at this location"
+    else
+      ln -s ${script_dir}/bin/run ${path}/run
+      echo "Created link to 'run'"
+    fi
     test=$(export | grep $PATH | grep ${path} > /dev/null)
     if [[ ${test} -ne 0 ]]; then
       echo "The installed path (${path}) does not appear in you \$PATH environment variable"
       echo "Please include the followng line in your shell's startup script (e.g. .bashrc, .zshrc)"
       echo "export PATH=\"\${PATH}:${path}\""
     fi
+    echo ""
+    echo "The helper script can be uninstalled by running:"
+    echo "rm ${path}/run"
+    echo ""
+    echo "The helper script is automatically updated along with this repository."
+    echo "Update using: git pull"
     exit 0
   fi
 done
