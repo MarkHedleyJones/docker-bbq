d45c68c07eac5772f32a3d5f1ca680ecbb4266b8
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-04-03 22:10:22 +0900

    Add production build stage to template repositories

--- run_tests.sh
+++ run_tests.sh
@@ -2,32 +2,30 @@
 
 path_base="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd -P)"
 path_test="${path_base}/tests"
-dirname_workspace="workspace"
 
-# Outer repo containes the image that executes the tests
-dirname_test_repo="test_repo"
-path_test_repo="${path_test}/${dirname_test_repo}"
-path_test_repo_workspace="${path_test_repo}/${dirname_workspace}"
-
-
-# Use the default template as the test container
-cd "${path_test}" || exit 1
-"${path_base}"/bin/create_docker_repo "${dirname_test_repo}" > /dev/null
-
-# Setup and build the outer test repository
-cd "${path_test_repo}" || exit 1
-make > /dev/null
-
-# Copy in the 'run' script and a success target
-cat << EOF > "${path_test_repo_workspace}"/target.sh
+create_repo() {
+  # Use the default template as the test container
+  cd "${path_test}" || exit 1
+  "${path_base}"/bin/create_docker_repo "$1" > /dev/null
+  cd "$1" || exit 1
+  # Copy in the 'run' script and a success target
+  cat << EOF > workspace/target.sh
 #!/usr/bin/env sh
 
 echo "SUCCESS"
 EOF
-chmod +x "${path_test_repo_workspace}"/target.sh
+  chmod +x workspace/target.sh
+}
 
-"${path_test}"/tests.sh "${path_test}" "${dirname_test_repo}"
+create_repo "test_repo_lite"
+make > /dev/null
+
+create_repo "test_repo_production"
+make production > /dev/null
+
+"${path_test}"/tests.sh "${path_test}" "test_repo_lite" "test_repo_production"
 
 # Clean-up tests
-docker image rm "${dirname_test_repo}" > /dev/null
-rm -rf "${path_test_repo}"
+docker image rm "test_repo_lite" > /dev/null
+docker image rm "test_repo_production" > /dev/null
+rm -rf "${path_test}/test_repo_lite" "${path_test}/test_repo_production" "${path_test}/log.txt"
--- tests/tests.sh
+++ tests/tests.sh
@@ -4,17 +4,17 @@ path_run=NULL
 
 run_test() {
   printf " - "
-  printf "%-30s" "${1}"
+  printf "%-31s" "${1}"
   printf " ... "
   shift
-  "${path_run}"/run "$@" > "${path_test_repo}"/log.txt
-  if [[ "$(cat "${path_test_repo}/log.txt" | xargs)" =~ "SUCCESS" ]]; then
+  "${path_run}"/run "$@" > "${path_test}"/log.txt
+  if [[ "$(cat "${path_test}/log.txt" | xargs)" =~ "SUCCESS" ]]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILED\e[0m"
     echo ""
     echo -e "\e[33mERROR MESSAGE:\e[0m"
-    cat "${path_test_repo}"/log.txt
+    cat "${path_test}"/log.txt
     echo ""
     echo -e "\e[33mFAILING COMMAND:\e[0m"
     echo "run $@"
@@ -25,11 +25,13 @@ run_test() {
   fi
 }
 
-if [[ $# -eq 2 ]]; then
-  path_test=$1
+if [[ $# -eq 3 ]]; then
+  path_test="$1"
   path_run="$(cd "${path_test}" && cd ../bin && pwd)"
-  dirname_test_repo=$2
-  path_test_repo="${path_test}/${dirname_test_repo}"
+  repo_lite_name="$2"
+  repo_production_name="$3"
+  path_repo_lite="${path_test}/${repo_lite_name}"
+  path_repo_production="${path_test}/${repo_production_name}"
 else
   echo "This file should be run by the 'run_tests.sh' script."
 fi
@@ -43,16 +45,19 @@ echo "Running path resolution tests:"
 # Setup by moving to a location
 # Then run the test by passing a name and expected command
 
-cd "${path_test_repo}" || exit 1
-run_test "inside repo, outside workspace" workspace/target.sh
+cd "${path_repo_lite}" || exit 1
+run_test "inside lite repo, outside workspace" workspace/target.sh
 
-cd "${path_test_repo}/workspace" || exit 1
-run_test "inside repo, inside workspace" ./target.sh
-
-cd "${path_test}" || exit 1
-run_test "outside repo, single command" "${path_test_repo}"/workspace/target.sh
+cd "${path_repo_lite}/workspace" || exit 1
+run_test "inside lite repo, inside workspace" ./target.sh
 
 cd "${path_test}" || exit 1
-run_test "outside repo, split command" "${path_test_repo}" /workspace/target.sh
+run_test "outside lite repo, single command" "${path_repo_lite}"/workspace/target.sh
+
+cd "${path_test}" || exit 1
+run_test "outside lite repo, split command" "${path_repo_lite}" /workspace/target.sh
+
+cd / || exit 1
+run_test "production image's workspace" "${path_repo_production}" /workspace/target.sh
 
 echo "All tests passed"
