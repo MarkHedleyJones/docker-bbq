2e5b1af1f4f54400dec62f29d37694075579470d
Mark Hedley Jones <markhedleyjones@gmail.com>
2021-05-08 11:58:06 +0900

    Add python template

---
+++ templates/repositories/python/default.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+tag=${tag:-3-buster}
+
+dockerfile_components=(
+  file-header
+  install-apt-packages
+  install-pip-packages
+  copy-local-resources
+  target-development
+  target-development-body
+  target-preproduction
+  target-preproduction-body
+  target-production
+  target-production-body
+)
+
+makefile_components=(
+  file-header
+  buildstage-development
+  buildstage-development-body
+  buildstage-production
+  buildstage-production-body
+  buildstage-prebuild
+  buildstage-prebuild-body
+  buildstage-postbuild
+  buildstage-postbuild-body
+)
+
+readme_components=(
+  file-header
+  section-building
+)
+
+create_directories=()
+
+dockerignore_lines=()
+
+gitignore_lines=()
