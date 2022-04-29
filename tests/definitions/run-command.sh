bc0bfdab08c1fa8ef262e5c8b1f4fd59f388cd7c
Mark Hedley Jones <MarkHedleyJones@gmail.com>
2022-04-29 18:50:13 +0900

    run-command: Remove `run` tests in production images (drop-support)

--- tests/definitions/run-command.sh
+++ tests/definitions/run-command.sh
@@ -24,20 +24,3 @@ pass "run ./target.sh | grep boom"
 cd /tmp
 name="Run target outside repository"
 pass "run --repo ${repo_path} workspace/target.sh | grep boom"
-
-
-# Build production image with target script
-cd ${repo_path}
-make production > /dev/null 2>&1
-rm workspace/target.sh
-subheading "Production builds"
-
-name="Run command in container"
-pass "run rm --help | grep 'Remove (unlink) the FILE(s).'"
-
-name="Run target from inside repository"
-pass "run workspace/target.sh | grep boom"
-
-cd /tmp
-name="Run target outside repository"
-pass "run --repo ${repo_path} workspace/target.sh | grep boom"
