a569e76eb55c07869fc546a78128bcae8186921b
Mark Hedley Jones <markhedleyjones@gmail.com>
2025-07-29 15:31:01 +0900

    Add cm test to existing ros1 template

--- tests/definitions/templates/catkin_make.sh
+++ tests/definitions/templates/catkin_make.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-name="Catkin build"
+name="Catkin build (system)"
 pass 'cd catkin_ws && run catkin_make'
+
+name="Catkin build (custom cm script)"
+# Test cm script from project root - it should work from any directory
+pass 'cd .. && run cm'
